49359ee3174eb5a157a66afbac8f1f4d33621836
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-02-21 15:47:49 -0600

    adds test template

---
+++ git_lab.py
@@ -0,0 +1,13 @@
+import math
+
+def add_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def subtract_numbers(a, b):
+    """Returns the difference of two numbers."""
+    return a - b
+
+def multiply_numbers(a, b):
+    """Returns the product of two numbers."""
+    return a / b
---
+++ test_git_lab.py
@@ -0,0 +1,23 @@
+import unittest
+from git_lab import (
+    calculate_circle_area,
+    celsius_to_fahrenheit,
+    is_palindrome,
+    calculate_triangle_area,
+    is_prime,
+    add_numbers,
+    subtract_numbers
+)
+import math
+
+class TestUtils(unittest.TestCase):
+
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+    def test_subtract_numbers(self):
+        self.assertEqual(subtract_numbers(2, 3), -1)
+        self.assertEqual(subtract_numbers(-1, 1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
