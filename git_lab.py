ee7cb3e5b459d5e61b02fc6bc536d03f92b126b7
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-02-21 15:51:18 -0600

    complets add_positive_numbers with tests

--- git_lab.py
+++ git_lab.py
@@ -11,3 +11,7 @@ def subtract_numbers(a, b):
 def multiply_numbers(a, b):
     """Returns the product of two numbers."""
     return a / b
+
+def add_positive_numbers(a):
+    """Rteruns the sum of all positive numbers in the list a"""
+    return sum(x for x in a if x > 0)
--- test_git_lab.py
+++ test_git_lab.py
@@ -6,7 +6,9 @@ from git_lab import (
     calculate_triangle_area,
     is_prime,
     add_numbers,
-    subtract_numbers
+    subtract_numbers,
+    add_positive_numbers
+
 )
 import math
 
@@ -18,6 +20,9 @@ class TestUtils(unittest.TestCase):
     def test_subtract_numbers(self):
         self.assertEqual(subtract_numbers(2, 3), -1)
         self.assertEqual(subtract_numbers(-1, 1), -2)
+    def test_add_positive_numbers(self):
+        self.assertEqual(add_positive_numbers([1, -2, 3, 4]), 8)
+        self.assertEqual(add_positive_numbers([-1, -2, -3]), 0)
 
 if __name__ == '__main__':
     unittest.main()
